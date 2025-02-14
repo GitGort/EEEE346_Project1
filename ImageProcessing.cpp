1a4114e798745c8102879fd8bc317e3a75e00187
GitGort <ger2567@g.rit.edu>
2025-02-13 22:32:26 -0500

    Edge detect functional
    Only the first three functions remain

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -76,14 +76,54 @@ void medianFilter(int **oData, int **rData, int height, int width) {
 
 void edgeDetect(int **oData, int **rData, int height, int width) {
   // X-direction filter
-  int Fx[3][3] = {{-1, 0, 1}, 
-                  {-2, 0, 2},
-                  {-1, 0, 1}};
+  int Fx[9] = {-1, 0, 1, 
+               -2, 0, 2,
+               -1, 0, 1};
   
   // Y-direction filter
-  int Fy[3][3] = {{ 1,  2,  1}, 
-                  { 0,  0,  0},
-                  {-1, -2, -1}};
+  int Fy[9] = { 1,  2,  1, 
+                0,  0,  0,
+               -1, -2, -1};
 
   // TODO: write your code
+  // this loop moves the grid down through rows
+  for (int row = 1; row < height - 1; row++)
+  {
+    // this loop moves the grid across the columns
+    for (int column = 1; column < width - 1; column++)
+    {
+        // copy the 3x3 grid of values to a one dimensional array
+        int group[9] =  {oData[row-1][column-1], oData[row-1][column], oData[row-1][column+1],
+                        oData[row][column-1], oData[row][column], oData[row][column+1],
+                        oData[row+1][column-1], oData[row+1][column], oData[row+1][column+1]};
+        // Create variables to store Gx and Gy
+        int Gx = 0, Gy = 0;
+        // multiply the adjacent values by the filters
+        for (int element = 0; element < 9; element++)
+        {
+           Gx += group[element] * Fx[element];
+           Gy += group[element] * Fy[element];
+        }
+        // calculate base of the root exp
+        int base = (Gx * Gx) + (Gy * Gy);
+        // calculate G
+        int G = static_cast<int>(sqrt(base));
+        // use min function to prevent >255 to be output
+        rData[row][column] = min(255,G);
+    }
+  }
+  //copy edge data to output
+  //first loop for left/right sides
+  for (int row = 0; row < height; row++)
+  {
+    rData[row][0] = oData[row][0];
+    rData[row][width-1] = oData[row][width-1];
+  }
+  
+  //second loop for top/bottom
+  for (int column = 0; column < width; column++)
+    {
+        rData[0][column] = oData[0][column];
+        rData[height-1][column] = oData[height-1][column];
+    }
 }
